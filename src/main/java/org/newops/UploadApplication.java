8a2265319e3b0e0844586c38ff18951a4f3539bd
addepallimadhu <49113120+addepallimadhu@users.noreply.github.com>
2020-12-16 15:45:42 +0530

    Add files via upload

---
+++ src/main/java/org/newops/UploadApplication.java
@@ -0,0 +1,15 @@
+package org.newops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+public class UploadApplication {
+    public static void main(String[] args){
+        SpringApplication.run(UploadApplication.class,args);
+    }
+}
---
+++ src/main/java/org/newops/files/excel/loader/WDMSecuritiesLoader.java
@@ -0,0 +1,77 @@
+package org.newops.files.excel.loader;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.newops.model.Security;
+import org.newops.model.Holding;
+import org.newops.model.Fund;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class WDMSecuritiesLoader {
+    public static String TYPE = "text/csv";
+    static String[] HEADERs = { "Id", "Title", "Description", "Published" };
+
+    private static final org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(WDMSecuritiesLoader.class);
+    public static boolean hasCSVFormat(MultipartFile file) {
+
+        if (!TYPE.equals(file.getContentType())) {
+            return false;
+        }
+
+        return true;
+    }
+
+
+    public static List<Security> csvToWDMSecurities(InputStream is) {
+        try (BufferedReader fileReader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+             CSVParser csvParser = new CSVParser(fileReader,
+                     CSVFormat.DEFAULT.withFirstRecordAsHeader().withIgnoreHeaderCase().withTrim());) {
+
+            List<Security> securities = new ArrayList<Security>();
+
+            Iterable<CSVRecord> csvRecords = csvParser.getRecords();
+
+            SimpleDateFormat formatter1=new SimpleDateFormat("mm/dd/yyyy");
+
+
+            for (CSVRecord csvRecord : csvRecords) {
+
+               logger.info("Procesing: " + csvRecord.get("ISIN NO."));
+
+               Security security = new Security();
+
+               security.IssueDescription = csvRecord.get("ISSUE_DESC");
+
+                if (csvRecord.get("ISSUE_DATE").compareTo("")==0)
+                    security.AllotmentDate = null;
+                else
+                    security.AllotmentDate  = formatter1.parse(csvRecord.get("ISSUE_DATE"));
+
+                if (csvRecord.get("MAT_DATE").compareTo("")==0)
+                    security.RedemptionDate = null;
+                else
+                    security.RedemptionDate  = formatter1.parse(csvRecord.get("MAT_DATE"));
+
+                security.ISIN = csvRecord.get("ISIN NO.");
+
+                securities.add(security);
+            }
+
+            return securities;
+        } catch (IOException | ParseException e) {
+            throw new RuntimeException("fail to parse CSV file: " + e.getMessage());
+        }
+    }
+
+}
