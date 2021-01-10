57a961facbd3d4124c82a29cabd401ac57b10a68
addepallimadhu <49113120+addepallimadhu@users.noreply.github.com>
2021-01-10 22:08:44 +0530

    Delete FundRepository.java

--- src/main/java/org/newops/repository/FundRepository.java
+++
@@ -1,9 +0,0 @@
-package org.newops.repository;
-
-import org.newops.model.Fund;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface FundRepository extends JpaRepository<Fund, Void>, JpaSpecificationExecutor<Fund> {
-
-}
