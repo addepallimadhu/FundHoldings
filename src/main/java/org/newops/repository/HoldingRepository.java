d29e9d132c4eca9d205e68dba9b231591db5cf3d
addepallimadhu <49113120+addepallimadhu@users.noreply.github.com>
2021-01-10 22:08:54 +0530

    Delete HoldingRepository.java

--- src/main/java/org/newops/repository/HoldingRepository.java
+++
@@ -1,9 +0,0 @@
-package org.newops.repository;
-
-import org.newops.model.Holding;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface HoldingRepository extends JpaRepository<Holding, Void>, JpaSpecificationExecutor<Holding> {
-
-}
